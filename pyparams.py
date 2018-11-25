8396c05e0bce840742f59e1e3a120b46c9d510a4
yangdan <bupt-steven@foxmail.com>
2018-11-25 13:57:03 +0800

    upload the params

---
+++ pyparams.py
@@ -0,0 +1,10 @@
+def get_params():
+    params = []
+    # your indicator
+    video_type = 1
+    #------- fill your params ------
+
+    # ------------------------------ 
+    params.append(video_type)
+    return params 
+
